849dab96e281cc05f9f5371a0ef07c7d3d6e7fd6
Ramdhas Ueikattan <ramdhas.ueikattan@syncfusion.com>
2017-08-30 16:42:10 +0530

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { gridData } from './data';
 @Component({
     selector: 'my-app',
-    template: `<ej-grid #grid [dataSource]='data' [allowSorting]='true'>
+    template: `<ej-grid #grid [dataSource]='data' [allowSorting]='true' gridLines='both'>
                     <e-columns>
                         <e-column field='LastName' headerText='Last Name' width='140'></e-column>
                         <e-column field='FirstName' headerText='First Name' width='170' format= 'C2'></e-column>
@@ -11,9 +11,7 @@ import { gridData } from './data';
                         <e-column field='BirthDate' headerText='Bitth Date' width='120' format='yMd' ></e-column>
                         <e-column field='City' headerText='City' width='120' ></e-column>
                     </e-columns>
-                </ej-grid>`,
-    styleUrls: ['app/app.style.css'],
-    encapsulation: ViewEncapsulation.None
+                </ej-grid>`
 })
 export class AppComponent implements OnInit {
 
